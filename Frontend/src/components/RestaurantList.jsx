a0458bb445165bdef5ce329b54288c1fd73372cf
aahektor <aronhektor7@gmail.com>
2026-02-11 12:51:02 +0100

    feat: Add delivery cost display to restaurant cards

--- Frontend/src/components/RestaurantList.jsx
+++ Frontend/src/components/RestaurantList.jsx
@@ -1,65 +1,83 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import DeliveryIcon from '../components/icons/DeliveryIcon'
 
 const RestaurantList = ({ selectedCategory }) => {
     const [restaurants, setRestaurants] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    
     useEffect(() => {
         fetch("http://localhost:5291/api/Restaurants")
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("Hämtade restauranger:", data);
-            setRestaurants(data);
-            setLoading(false);
-        })
-        .catch((error) => {
-            console.error("Kunde inte hämta data:", error);
-            setLoading(false);
-        });
+            // fetch("http://192.168.1.158:5291/api/Restaurants")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Hämtade restauranger:", data);
+                setRestaurants(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error("Kunde inte hämta data:", error);
+                setLoading(false);
+            });
     }, []);
 
     const filterRestaurants = restaurants.filter((restaurant) => {
-        if (selectedCategory === 'popular') return restaurant.isPopular === true;
+        if (selectedCategory === "popular")
+            return restaurant.isPopular === true;
 
-        return restaurant.category?.toLowerCase() == selectedCategory.toLowerCase();
+        return (
+            restaurant.category?.toLowerCase() == selectedCategory.toLowerCase()
+        );
     });
-    
+
     if (loading) {
         return (
             <p className="text-center text-gray-500">Laddar restauranger...</p>
         );
     }
 
+
     return (
         <div className="flex flex-col gap-4 pb-20">
-            {filterRestaurants.map((restaurant) => (
-                <Link 
-                    to={`/restaurant/${restaurant.id}`}
-                    key={restaurant.id}
-                    className="rounded-3xl overflow-hidden outline-1 outline-slate-200/50"
-                >
-                    <div>
-                        <img
-                            src={
-                                restaurant.imageUrl &&
-                                restaurant.imageUrl.length > 10
-                                    ? restaurant.imageUrl
-                                    : "https://worldfoodtour.co.uk/wp-content/uploads/2013/06/neptune-placeholder-48.jpg"
-                            }
-                            alt="Restaurant Image"
-                            className="w-full h-40 object-cover"
-                        />
-                    </div>
-                    <div className="p-3 bg-white">
-                        <h3 className="font-bold">{restaurant.name}</h3>
-                        <p>{restaurant.address}</p>
+            {filterRestaurants.map((restaurant) => {
 
-                      
-                    </div>
-                </Link>
-            ))}
+                const deliveryCost = restaurant.id % 2 === 0 ? 0 : 49;
+
+                return (
+                    <Link
+                        to={`/restaurant/${restaurant.id}`}
+                        key={restaurant.id}
+                        className="rounded-3xl overflow-hidden outline-1 outline-slate-200/50"
+                    >
+                        <div>
+                            <img
+                                src={
+                                    restaurant.imageUrl &&
+                                    restaurant.imageUrl.length > 10
+                                        ? restaurant.imageUrl
+                                        : "https://worldfoodtour.co.uk/wp-content/uploads/2013/06/neptune-placeholder-48.jpg"
+                                }
+                                alt="Restaurant Image"
+                                className="w-full h-40 object-cover"
+                            />
+                        </div>
+                        <div className="p-3 bg-white">
+                            <h3 className="font-bold text-lg">{restaurant.name}</h3>
+                            <p>{restaurant.address}</p>
+                        </div>
+                        <div className=" bg-white px-4">
+                            <div className="border-t border-dashed border-gray-300 py-2 flex gap-2">
+                                <DeliveryIcon className="w-4 h-4 text-green-900/60 self-center" />
+                                {deliveryCost === 0 ? (
+                                    <span className="text-green-500 text-sm">Free Delivery</span>
+                                ) : (
+                                    <span className="text-slate-500 text-sm">49kr</span>
+                                )}
+                            </div>
+                        </div>
+                    </Link>
+                )
+        })}
         </div>
     );
 };
---
+++ Frontend/src/components/icons/DeliveryIcon.jsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+const SvgComponent = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 48 48"
+    {...props}
+  >
+    <g data-name="Layer 2">
+      <path fill="none" d="M0 0h48v48H0z" data-name="invisible box" />
+      <path
+        fill="currentColor"
+        d="M35.8 11a3.2 3.2 0 0 0-2.2-1H32V8a2.9 2.9 0 0 0-3-3H5a2.9 2.9 0 0 0-3 3v27a2.9 2.9 0 0 0 3 3h2.3a7 7 0 0 0 13.4 0h6.6a7 7 0 0 0 13.4 0H43a2.9 2.9 0 0 0 3-3V22.2Zm-2.7 3 7.3 8H32v-8ZM14 39a3 3 0 1 1 3-3 2.9 2.9 0 0 1-3 3Zm20 0a3 3 0 1 1 3-3 2.9 2.9 0 0 1-3 3Z"
+        data-name="Layer 7"
+      />
+    </g>
+  </svg>
+)
+export default SvgComponent
