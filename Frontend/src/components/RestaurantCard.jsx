54e3f48fbb85c080dd0c400aa6805168cbbce083
aahektor <aronhektor7@gmail.com>
2026-02-15 18:34:13 +0100

    feat: add ratings to restaurantcards with coloring logic

--- Frontend/src/components/RestaurantCard.jsx
+++ Frontend/src/components/RestaurantCard.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 
 import { Link } from "react-router-dom";
 import DeliveryIcon from '../components/icons/DeliveryIcon'
 
 import HeartIcon from '../components/icons/HeartIcon'
+import StarIcon from '../components/icons/StarIcon'
 
 
 const RestaurantCard = ({ restaurant }) => {
     const [isLiked, setIsLiked ] = useState(false);
     const deliveryCost = restaurant.id % 2 === 0 ? 0 : 49;
+
+    const stars = useMemo(() => {
+        const seed = (restaurant.id * 1337) % 31; // Lite "matte-magi"
+        return parseFloat((2 + (seed / 10)).toFixed(1));
+    }, [restaurant.id]);
+
 
   return (
     <Link
@@ -17,7 +24,7 @@ const RestaurantCard = ({ restaurant }) => {
         className="rounded-3xl overflow-hidden outline-1 outline-slate-200/50 shadow-2xs"
     >
         <div className="relative">
-            <div className="w-10 h-10 absolute right-3 top-3 bg-gray-200 rounded-full flex justify-center items-center"
+            <div className="w-10 h-10 absolute right-3 top-3 bg-gray-100 rounded-full flex justify-center items-center"
                 onClick={ (e) => {
                     e.preventDefault();
                     e.stopPropagation();
@@ -25,7 +32,7 @@ const RestaurantCard = ({ restaurant }) => {
                 }}
             >
                 <HeartIcon 
-                className={`h-5 w-5 ${isLiked ? "text-red-500 fill-current" : "text-gray-500"}`} />
+                className={`h-5 w-5 ${isLiked ? "text-red-500 fill-current" : "text-gray-400"}`} />
             </div>
             <img
                 src={
@@ -39,11 +46,15 @@ const RestaurantCard = ({ restaurant }) => {
             />
         </div>
         <div className="p-3 bg-white">
-            <div>
+            <div className='flex justify-between'>
                 <h3 className="font-bold text-lg">{restaurant.name}</h3>
-                <span></span>
+                <span className={` flex flex-row gap-1 justify-center items-center px-1.5 h-5 rounded-md text-sm
+                    ${stars >= 3.0 ? 'bg-green-300/60 text-green-600' : 'bg-gray-200/60 text-black'}
+                    `}>
+
+                    {stars}
+                    <StarIcon className={`w-3.5 h-3.5 self-center font-light ${stars >= 3.0 ? 'text-green-600' : 'text-yellow-600'} `} /></span>
             </div>
-            <p>{restaurant.address}</p>
         </div>
         <div className=" bg-white px-4">
             <div className="border-t border-dashed border-gray-300 py-2 flex gap-2">
